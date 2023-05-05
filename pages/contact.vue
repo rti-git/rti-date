903dd9a8af21b101d521b4c400acfd8210af82cd
Easyfun <easyfun@EasyfundeMacBook-Pro.local>
2023-05-05 14:47:57 +0800

    First dear

---
+++ pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="relative isolate flex items-center justify-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5">
+  <div class="flex flex-wrap items-center pr-12 gap-x-4 gap-y-2">
+  <NuxtLink to="/">
+    <button class="btn btn-primary">首頁</button>
+  </NuxtLink>  
+  <NuxtLink to="/send">
+    <button class="btn btn-primary">參加辦法</button>
+  </NuxtLink>
+  <NuxtLink to="/notes">
+    <button class="btn btn-primary">注意事項</button>
+  </NuxtLink>
+   <NuxtLink to="/contact">
+    <button class="btn btn-primary">聯絡我們</button>
+  </NuxtLink>
+  </div>
+  </div>
+  <div class="container p-24">
+    <h1 class="text-6xl">聯絡我們</h1>
+
+    <ul class="list-disc text-2xl indent-8 p-8">
+      <li>聯絡人：《Dear Rti,》明信片寄情徵集活動小組</li>
+      <li>電話：886-2-2885-6168分機724-726</li>
+      <li>E-mail：dearrti@gmail.com</li>
+    </ul>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="h-screen bg-red-100">
+  <header class="absolute inset-x-0 top-0 z-50">
+    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <a href="https://www.rti.org.tw/" target="_blank" class="-m-1.5 p-1.5">
+          <span class="sr-only">Your Company</span>
+          <img class="h-8 w-auto" src="https://topic.rti.org.tw/sns/wp-content/uploads/2020/04/960x960-1-1.jpg" alt="">
+        </a>
+      </div>
+
+      <!-- <div class="hidden lg:flex lg:gap-x-12">
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
+      </div>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+      </div> -->
+    </nav>
+
+  </header>
+
+  <div class="relative isolate px-6 pt-14 lg:px-8">
+    <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+      <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div>
+    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+      </div>
+      <div class="text-center">
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">RTI</h1>
+        <p class="mt-6 text-lg leading-8 text-gray-600">央廣大生日</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+        <NuxtLink class="hover:bg-indigo-500 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" to="/send">
+          開始填寫
+        </NuxtLink>  
+        </div>
+      </div>
+    </div>
+    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+      <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div>
+  </div>
+</div>
+<div class="container py-12">
+        <h1 class="text-xs px-4">Dear Rti,明信片寄情徵活動</h1>
+        <p>Dear Rti, 生日快樂！</p><br>
+        <p>Dear Rti, 我希望戰爭早日結束，世界和平。</p><br>
+        <p>因應當前國際情勢，並適逢央廣95周年臺慶，央廣將藉由徵集全球聽網友明信片，將個人小願小力，匯聚化成大願大力，在祝福央廣95周年生日快樂同時，亦寫下對世界和平、生活安定的期許與祝福。</p>
+
+        <span>一張明信片，一份念力！</span>
+
+        <p>你寄來的明信片，將有機會被世界看見及聽見～</p>
+        <span>⭐在台灣各地巡迴展出</span><span>⭐在央廣臺慶晚宴展示</span><span>⭐在央廣廣播節目中唸出你的祝福</span><span>⭐在央廣各語社群平台進行互動活動</span>
+
+        <p>凡寄達明信片並符合徵集內容者，央廣皆回寄95周年專屬限量明信片乙張。除此之外，更有機會獲得95周年限量獨家好禮！</p>
+        <p>來吧!<br>現在就拿起筆，寫下你的祝福吧！</p>
+    </div>
+</template>
