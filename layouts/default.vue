d1a5f23b951acf5d68300f48b99c1ebd08e92eb8
Easyfun <easyfun@EasyfundeMacBook-Pro.local>
2023-05-23 01:17:28 +0800

    new pages

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <Home />
+    <Content />
+    <Intr />
+    <Form /> 
+    <Footer />
+    <slot />
+</template>
+
+<style scoped>
+li{
+    font-family: Arial, Helvetica, sans-serif;
+}
+</style>
