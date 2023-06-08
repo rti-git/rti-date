1f4307be6a08fd4829d6752fab981892c6f9043c
Easyfun <easyfun@EasyfundeMacBook-Pro.local>
2023-06-08 17:13:34 +0800

    Chang bg2

--- components/Content.vue
+++ components/Content.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex text-2xl md:flex-wrap-reverse lg:text-4xl bg-rti_ing_feet font-bold gap-8 p-16 px-8 sm:px-16 lg:px-64">
+    <div class="flex text-2xl md:flex-wrap-reverse lg:text-4xl bg-rti_ing_green font-bold gap-8 p-16 px-8 sm:px-16 lg:px-64">
         <p class="text-rti_ing_white">相逢自是有緣，新聞時事性節目「兩岸ing」自2013年開播至今，目前每週一至週五18:00~18:30在央廣播出。為感謝聽友長期的收聽與支持，節目特別舉辦「相逢十年 兩岸ING抽好禮」活動。</p>
     </div>
 </template>
