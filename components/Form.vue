d5100e09f3b382c20d61f1bc412bdca24eeeab78
Easyfun <easyfun@EasyfundeMacBook-Pro.local>
2023-08-29 13:36:43 +0800

    Fix size

--- components/Form.vue
+++ components/Form.vue
@@ -2,12 +2,12 @@
     <div class="flex flex-col text-rti_spot_title bg-rti_spot_bg text-2xl md:text-4xl md:gap-2 gap-8 p-16 px-8 sm:px-16 lg:px-64">
         <h2 class="text-4xl font-bold">活動遊戲：</h2>
     <div class="flex flex-col md:flex-row justify-between items-center p-12 pb-8 lg:px-16">
-        <div class="flex  text-3xl w-auto sm:w-1/2 md:w-1/2 pt-10">
+        <div class="flex  text-3xl w-auto sm:w-1/2 md:w-1/3 pt-10">
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSdARvdvgaF96GeYd9uRm9McVHOUSb2qXeuO1C0LWAXIVO2Wsg/viewform" target="_blank">
-                <img src="../assets/images/gf_icon.png" class="pb-0 md:pl-0 md:pb-16 scale-100 hover:scale-110 ease-in duration-200" alt="">
+                <img src="../assets/images/gf_icon.png" class="pb-0 md:pl-0 scale-100 hover:scale-110 ease-in duration-200" alt="">
             </a>
         </div>
-        <div class="flex text-3xl w-auto sm:2/2  md:w-1/2 pt-10">
+        <div class="flex text-3xl w-auto sm:2/2  md:w-1/3 pt-10">
             <a href="https://wj.qq.com/s2/12994009/6f4a/" target="_blank">
                 <img src="../assets/images/T_icon.png" class="scale-100 hover:scale-110 ease-in duration-200" alt="">
             </a>
--- components/Gift.vue
+++ components/Gift.vue
@@ -2,22 +2,22 @@
     <div class="flex flex-col bg-rti_spot_bg h-auto  text-4xl p-16 sm:px-16 lg:px-64">
          <h2 class="font-bold text-rti_spot_title pb-8 ">活動贈品：</h2>
         <div class="flex flex-col text-center  md:flex-row h-3/5  lg:px-68 justify-between  pb-8 text-rti_ing_white">
-            <div class="w-auto md:w-1/3 lg:w-auto">
-                <img class="h-80 pb-0  lg:pb-8"  src="../assets/images/gif.png" alt="">
+            <div class="md:w-1/3 lg:w-auto">
+                <img class="h-auto md:h-64 lg:h-80 pb-0  lg:pb-8"  src="../assets/images/gif.png" alt="">
                 <div class="text-2xl">
                     <p>台灣文創紙相機</p>
                     <p>3個</p>
                 </div>
             </div>
-            <div class="w-auto md:w-1/3 lg:w-auto">
-                <img class="h-80 pb-0  lg:pb-8" src="../assets/images/gif.png" alt="">
+            <div class="md:w-1/3 lg:w-auto md:ml-8">
+                <img class="h-auto md:h-64  lg:h-80 pb-0  lg:pb-8" src="../assets/images/gif.png" alt="">
                 <div class="text-2xl">
                     <p>台灣文創木製象棋</p>
                     <p>3個</p>
                 </div>
             </div>
-            <div class="w-auto md:w-1/3 lg:w-auto">
-                <img  class="h-80 pb-0  lg:pb-8" src="../assets/images/gif.png" alt="">
+            <div class="md:w-1/3 lg:w-auto md:ml-8">
+                <img  class="h-auto md:h-64  lg:h-80 pb-0  lg:pb-8" src="../assets/images/gif.png" alt="">
                 <div class="text-2xl">
                     <p>故宮平安富貴鋼筆禮盒</p>
                     <p>2個</p>
