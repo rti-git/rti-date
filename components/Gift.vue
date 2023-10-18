cb9935e10e00d330726a8f25a573e9c1b55d2197
Easyfun <easyfun@EasyfundeMacBook-Pro.local>
2023-10-18 16:08:30 +0800

    Fix word

--- components/Gift.vue
+++ components/Gift.vue
@@ -26,7 +26,7 @@
         </div>
     </div>
       <div class="wrap flex flex-col items-center pt-8 pb-8">
-        <h2 class="text-4xl text-red-600">臺灣新玩法有獎徵答活動-得獎名單</h2>
+        <h2 class="text-4xl text-red-600">臺灣心玩法有獎徵答活動-得獎名單</h2>
         <!--<p class="text-2xl pt-12">恭喜得獎人：</p> -->
       </div>
   <div class="flex flex-col md:flex-row relative overflow-x-auto justify-center w-full sm:px-16 lg:px-8">
